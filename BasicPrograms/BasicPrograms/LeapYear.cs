4e0586e7ca51499bc36b00fedc4ca24644afb002
shaik ameer basha <shaikshabana758@gmail.com>
2022-01-25 16:17:53 +0530

    [shabana] added LeapYear

--- BasicPrograms/BasicPrograms/LeapYear.cs
+++ BasicPrograms/BasicPrograms/LeapYear.cs
@@ -6,18 +6,17 @@ using System.Threading.Tasks;
 
 namespace BasicPrograms
 {
-    internal class Program
+    internal class LeapYear
     {
-        static void Main(string[] args)
+        public void Leap()
         {
-
-            Console.WriteLine("hello world");
             Console.WriteLine("Enter Year : ");
             int Year = int.Parse(Console.ReadLine());
             if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0)) Console.WriteLine("{0} is a Leap Year.", Year);
             else Console.WriteLine("{0} is not a Leap Year.", Year);
             Console.ReadLine();
 
+
         }
-    }
-}
+
+    }   }       
